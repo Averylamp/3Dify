89eb0be0613bc57969b51f9bd70ca7ef9519fdf0
Arlene Siswanto <arlenesiswanto@gmail.com>
2020-02-15 14:40:47 -0800

    Slicing logic - first

--- server/3Dify_algorithm.py
+++ server/3Dify_algorithm.py
@@ -2,9 +2,76 @@ import math
 import numpy as np
 
 
-# Returns the aggregate distance between two point clouds
+
+def point_cloud_distance(cloud_1, cloud_2):
+	raise('Point cloud distance is not yet implemented.')
+
+def choose_y_slice(cloud_1, cloud_2, step_size=4):
+	cloud_1 = sorted(cloud_1, key=lambda y: y[4])
+	cloud_2 = sorted(cloud_2, key=lambda y: y[4])
+
+	low_1 = min(cloud_1, key=lambda y: y[4])
+	high_1 = max(cloud_1, key=lambda y: y[4])
+	low_2 = min(cloud_2, key=lambda y: y[4])
+	high_2 = max(cloud_2, key=lambda y: y[4])
+
+	# TODO: figure out what granularity to use if sizes are disproportional
+	slice_size = min(high_1 - low_1, high_2 - low_2) / step_size
+
+	min_distance = math.inf
+
+	i = 1
+	while i * slice_size < (high_1 - low_1) + (high_2 - low_2):
+		included_1 = [p for p in cloud_1 if p[4] < cloud_1[0][4] + i * slice_size]
+		included_2 = [p for p in cloud_2 if p[4] > cloud_2[-1][4] - i * slice_size]
+
+		shift_1 = -included_1[0][4]
+		shift_2 = -included_2[0][4]
+		distance_y = point_cloud_distance(translate_x_y(included_1, y_shift=shift_1), translate_x_y(included_2, y_shift=shift_2))
+
+		if distance_y < min_distance:
+			min_distance = distance_y
+
+		i += 1
+
+	return min_distance
+
+
+
+def choose_x_slice(cloud_1, cloud_2, step_size=4):
+	cloud_1 = sorted(cloud_1, key=lambda x: x[3])
+	cloud_2 = sorted(cloud_2, key=lambda x: x[3])
+
+	low_1 = min(cloud_1, key=lambda x: x[3])
+	high_1 = max(cloud_1, key=lambda x: x[3])
+	low_2 = min(cloud_2, key=lambda x: x[3])
+	high_2 = max(cloud_2, key=lambda x: x[3])
+
+	# TODO: figure out what granularity to use if sizes are disproportional
+	slice_size = min(high_1 - low_1, high_2 - low_2) / step_size
+
+	min_distance = math.inf
+
+	i = 1
+	while i * slice_size < (high_1 - low_1) + (high_2 - low_2):
+		included_1 = [p for p in cloud_1 if p[3] < cloud_1[0][3] + i * slice_size]
+		included_2 = [p for p in cloud_2 if p[3] > cloud_2[-1][3] - i * slice_size]
+
+		shift_1 = -included_1[0][3]
+		shift_2 = -included_2[0][3]
+		distance_y = choose_y_slice(translate_x_y(included_1, x_shift=shift_1), translate_x_y(included_2, x_shift=shift_2), step_size)
+
+		if distance_y < min_distance:
+			min_distance = distance_y
+
+		i += 1
+
+	return min_distance
+
+
+# NAIVE - Returns the aggregate distance between two point clouds
 # O(n^2)
-def distance(cloud_1, cloud_2):
+def naive_distance(cloud_1, cloud_2):
 	total_distance = 0
 	for point in cloud_1:
 		smallest = None
@@ -17,6 +84,7 @@ def distance(cloud_1, cloud_2):
 		total_distance += min_cost
 	return total_distance
 
+# TODO: adjust weights
 # Input: points, represented as (r, g, b, x, y, z)
 #        weights to penalize rgb and physical distance differences
 #
