552db0b357a89ccbff428edbd5c9eda92525d7d1
Advait Maybhate <advait.maybhate9@gmail.com>
2020-02-15 01:24:48 -0800

    Add boilerplate process photo endpoint

--- server/main.py
+++ server/main.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def root():
+    return "3dify"
+
+@app.route("/upload/photo", methods=["POST"])
+def process_photo():
+    print(request.json)
+    return {"TODO": 0}
 
 if __name__ == "__main__":
     app.run()
