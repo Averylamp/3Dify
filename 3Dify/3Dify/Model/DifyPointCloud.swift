cac5e43cf5deafbf2660016ea6341b13a4f06615
rakeeb123 <rakeeb.hossain1@gmail.com>
2020-02-15 23:03:40 -0800

    backend conn

--- 3Dify/3Dify/Model/DifyPointCloud.swift
+++ 3Dify/3Dify/Model/DifyPointCloud.swift
@@ -21,8 +21,9 @@ struct PointCloudVertex {
   var colors: [UInt8] = []
   var width: Int = 0
   var height: Int = 0
-  
-  public func pointCloudNode() -> SCNNode {
+  let apiInstance = NetworkingDifyAPI()
+    
+    public func pointCloudNode(completion: ((SCNNode) -> Void)) {
     let points = self.pointCloud
     var vertices = Array(repeating: PointCloudVertex(x: 0, y: 0, z: 0, r: 0, g: 0, b: 0), count: points.count)
     
@@ -36,8 +37,11 @@ struct PointCloudVertex {
       vertices[i].b = Float(colors[i * 4 + 2]) / 255.0
     }
     
-    let node = buildNode2(points: vertices)
-    return node
+    apiInstance.sendImage(image: vertices, completion: {points in
+        // Process points w/ vertices
+        let node = self.buildNode2(points: vertices)
+        completion(node)
+    })
   }
   
   private func buildNode(points: [PointCloudVertex]) -> SCNNode {
